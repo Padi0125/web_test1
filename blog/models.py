52632d57ae0801127f994efedb191230123715e3
useung73 <useung73@gmail.com>
2022-11-22 14:43:14 +0900

    Post 모델을 생성하고 시간 설정하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)  # 제목은 30자 까지 지정
+    content = models.TextField()
+
+    # created_at = models.DateTimeField()  #수정 전 코드
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # author : 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
